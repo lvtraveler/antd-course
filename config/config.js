1b013f48c50d01b10dcaab5d69cc4ddc3ca7d3e5
lvtraveler <xiezhihai7@163.com>
2019-01-28 17:15:53 +0800

    基于Model的列表

--- config/config.js
+++ config/config.js
@@ -1,12 +1,30 @@
 export default {
-    singular: true,
+    // singular: true,
     plugins: [
         ['umi-plugin-react', {
-            antd:true
+            antd:true,
+            dva: true,
         }],
     ],
-    routes: [{
-        path: '/',
-        component: './Index',
-    }],
+    routes: [
+        {
+            path: '/',component: '../layout/index',
+            routes:[
+                // {path:'/',component:'HelloWorld'},
+                {path:'/helloworld',component:'HelloWorld'},
+                {path: '/', component: 'puzzlecards' },
+                {
+                    path:'/dashboard',
+                    routes:[
+                        { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+                        { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+                        { path: '/dashboard/workplace', component: 'Dashboard/Workplace' },
+                        
+                    ]
+            
+                },
+            ]
+        },
+        
+    ],
 };
--- src/page/Index.js
+++ src/page/Index.js
@@ -3,13 +3,14 @@ import React from 'React';
 import Content from './Content';
 import Clock from './Clock';
 
+
 //首页
 class Index extends React.Component {
     render() {
         return(
             <div>
-                <Clock></Clock>
-                <Content></Content>
+                {/* <Clock></Clock>
+                <Content></Content> */}
             </div>
         )
     } 
